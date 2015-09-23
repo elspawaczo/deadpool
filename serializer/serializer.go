86e6fb325f37bd25dd834369805638d78b25a097
Radek Los <radek.los@gmail.com>
2015-09-23 17:41:46 +0100

    Update report data structure

--- serializer/serializer.go
+++ serializer/serializer.go
@@ -3,14 +3,31 @@ package serializer
 import (
 	"encoding/json"
 	log "github.com/Sirupsen/logrus"
+	"net/http"
+	"time"
 )
 
+type Header struct {
+	http.Header
+}
+
 type Report struct {
-	HttpRequest  string `json:"request"`
-	HttpResponse string `json:"respond"`
-	Source       string `json:"source"`
-	Destination  string `json:"destination"`
-	Description  string `json:"description"`
+	Origin        string    `json:"origin",json"`
+	Method        string    `json:"method",json"`
+	Status        int       `json:"status",json"`
+	ContentType   string    `json:"content_type",json"`
+	ContentLength uint      `json:"content_length",json"`
+	Host          string    `json:"host",json"`
+	URL           string    `json:"url",json"`
+	Scheme        string    `json:"scheme",json"`
+	Path          string    `json:"path",path"`
+	Header        Header    `json:"header",json"`
+	Body          string    `json:"body",json"`
+	RequestHeader Header    `json:"request_header",json"`
+	RequestBody   string    `json:"request_body",json"`
+	DateStart     time.Time `json:"date_start",json"`
+	DateEnd       time.Time `json:"date_end",json"`
+	TimeTaken     time.Time `json:"time_taken",json"`
 }
 
 func UnmarshalReport(data []byte) (*Report, error) {
